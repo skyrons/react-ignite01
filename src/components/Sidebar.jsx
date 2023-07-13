397769efaa8ad08c800664b7e7c3be4bef74a600
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2023-07-13 20:19:50 -0300

    Avatar added at sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,28 @@
 import style from './Sidebar.module.css'
+import {PencilLine} from 'phosphor-react'
 
 export function Sidebar () {
     return (
         <aside className={style.sidebar}>
             <img src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80" 
-            alt="" 
-            className={style.cover} />
+                alt="" 
+                className={style.cover} 
+                />
 
             <div className={style.profile}>
+                <img 
+                    src="https://avatars.githubusercontent.com/u/57599226?v=4" 
+                    className={style.avatar} 
+                />
                 <strong>Hugo Souza</strong>
-                <span>React Developer</span>
+                <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
         
