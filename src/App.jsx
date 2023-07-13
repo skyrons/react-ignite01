f4862d2f151ebe0bbce7a70e6776b999de07159d
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2023-07-13 11:33:26 -0300

    Component Header changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
     <div>
       <Header />
       <Post 
-        produto="Enantato de Testosterona" 
-        content="Isso aqui é bomba viu ?"
+        produto="Post 1" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum hic earum maiores facere! Soluta rem consequuntur et incidunt quisquam cumque eum neque quos minus voluptatum voluptatem, excepturi eveniet dolorem."
         />
         <Post 
-        produto="PlayStation 5" 
+        produto="Post 2" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum hic earum maiores facere! Soluta rem consequuntur et incidunt quisquam cumque eum neque quos minus voluptatum voluptatem, excepturi eveniet dolorem."
         />
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import style from './Header.module.css'
+import style from './Header.module.css';
+
+import igniteLogo from '../assets/ignite-logo.svg';
 
 export function Header () {
     return (
         <header className={style.header}>
-            <strong > Ignite Feed </strong>
+            <img src={igniteLogo} alt="" />
         </header>
     );
 }
