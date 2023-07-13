a842b63e70b408752f020affb69999341dca09f0
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2023-07-13 14:45:42 -0300

    component sidebar wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,30 @@ import {Post} from './Posts'
 import { Header } from './components/Header.JSX'
 
 import './global.css'
+import style from './App.module.css'
+
 function App() {
 
   return (
     <div>
       <Header />
-      <Post 
-        produto="Post 1" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum hic earum maiores facere! Soluta rem consequuntur et incidunt quisquam cumque eum neque quos minus voluptatum voluptatem, excepturi eveniet dolorem."
-        />
-        <Post 
-        produto="Post 2" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum hic earum maiores facere! Soluta rem consequuntur et incidunt quisquam cumque eum neque quos minus voluptatum voluptatem, excepturi eveniet dolorem."
-        />
+
+      <div className={style.wrapper}>
+        <aside>
+          Sidebar
+        </aside>
+
+        <main>
+          <Post 
+          produto="Post 1" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum hic earum maiores facere! Soluta rem consequuntur et incidunt quisquam cumque eum neque quos minus voluptatum voluptatem, excepturi eveniet dolorem."
+          />
+          <Post 
+          produto="Post 2" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum hic earum maiores facere! Soluta rem consequuntur et incidunt quisquam cumque eum neque quos minus voluptatum voluptatem, excepturi eveniet dolorem."
+          />
+        </main>
+      </div>
     </div>
   )
 }
