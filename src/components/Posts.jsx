444cd4badde3dba69ed3782978b16ed4c2f620b1
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2023-07-17 13:19:23 -0300

    Comment form

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -21,18 +21,31 @@ export function Post () {
             </header>
             
             <div className={style.content}>
-            <p>Fala galeraa 👋</p>
+                <p>Fala galeraa 👋</p>
 
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-            <p>👉 <a href="#">jane.design/doctorcare</a></p>
+                <p>👉 <a href="#">jane.design/doctorcare</a></p>
 
-            <p> 
-                <a href="#">#novoprojeto</a>{' '}
-                <a href="#">#nlw</a>{' '} 
-                <a href="#">#rocketseat</a>{' '}
-            </p>
+                <p> 
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a href="#">#nlw</a>{' '} 
+                    <a href="#">#rocketseat</a>{' '}
+                </p>
             </div>
+
+            <form className= {style.commentForm}>
+
+                <strong>Deixe seu feedback</strong>
+
+                <textarea placeholder='Escreva um comentário...'></textarea>
+
+                <footer>
+                    <button type="submit">
+                        Publicar
+                    </button>
+                </footer>
+            </form>
         </article>
     )
 }
